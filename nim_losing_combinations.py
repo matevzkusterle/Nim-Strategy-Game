d5ea9bf3f12f7b2214b6558911813b60af409feb
Matevž Kusterle <82824561+matevzkusterle@users.noreply.github.com>
2025-02-02 22:14:11 +0100

    Add files via upload
    simple code that determines which combinations of board are losing

---
+++ nim_losing_combinations.py
@@ -0,0 +1,10 @@
+board = [3,5,7]
+
+def f():
+    s = []
+    for i in range(board[0]+1):
+        for j in range(board[1]+1):
+            for k in range(board[2]+1):
+                if i ^ j ^ k == 0:
+                    s.append([i,j,k])
+    return s
